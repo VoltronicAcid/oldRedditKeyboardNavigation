d3fe38943cfd8bae5f01ab08024fdc1a50e9c1f8
VoltronicAcid <78665774+VoltronicAcid@users.noreply.github.com>
2025-07-18 10:34:53 -0400

    gallery navigation

--- reddit.navigation.user.js
+++ reddit.navigation.user.js
@@ -2,7 +2,7 @@
 // @name          Old Reddit Keyboard Navigation
 // @description   Navigate old.reddit.com pages using arrow keys
 // @author        VoltronicAcid
-// @version       0.0.3
+// @version       0.1
 // @homepageURL   https://github.com/VoltronicAcid/oldRedditKeyboardNavigation
 // @supportURL    https://github.com/VoltronicAcid/oldRedditKeyboardNavigation/issues
 // @icon          https://www.google.com/s2/favicons?sz=64&domain=reddit.com
@@ -20,5 +20,29 @@
         if (key === "ArrowLeft" && prevButton) document.location = prevButton.href;
 
         if (key === "ArrowRight" && nextButton) document.location = nextButton.href;
+
+        if (key === "]") {
+            const previews = Array.from(document.querySelectorAll("div.gallery-preview"));
+
+            if (previews.filter(div => div.style.display === "").length) {
+                const selected = previews.filter(div => div.style.display === "")[0];
+                if (selected.nextElementSibling) {
+                    selected.style.display = "none";
+                    selected.nextElementSibling.style.display = "";
+                }
+            }
+        }
+
+        if (key === "[") {
+            const previews = Array.from(document.querySelectorAll("div.gallery-preview"));
+
+            if (previews.filter(div => div.style.display === "").length) {
+                const selected = previews.filter(div => div.style.display === "")[0];
+                if (selected.previousElementSibling && selected.previousElementSibling.className === "gallery-preview") {
+                    selected.style.display = "none";
+                    selected.previousElementSibling.style.display = "";
+                }
+            }
+        }
     };
 })();
